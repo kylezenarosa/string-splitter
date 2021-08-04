ea2dca20fd00c4a7e636bef12ba2704e00341326
Kyle Zenarosa <ksgzenarosa@yahoo.com>
2021-08-04 21:33:14 +0100

    cut5 - one string with space at beginning, expected array with one string and no spaces

--- tag_manipulator.py
+++ tag_manipulator.py
@@ -3,6 +3,7 @@ import re
 class TagManipulator():    
     def parse_string(self, tags, regex=""):
         result = []
+        tags = tags.strip()
 
         tempResult = re.split( regex, tags )
         if( len(tempResult[0]) > 0 ):
